b3f0c516f48877d9b715aa1e3e6653d044492b9a
igor-pokataev <ikpokataev@gmail.com>
2023-02-06 17:36:04 +0500

    up_test

--- git_test1.py
+++ git_test1.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 name = input('Whats your name?')
 print('Hello, ' , name,'!')
-print('We learn git and github')
+print('We learn git and github. That very good!')
 print('Our project:"Git and Github tutorial"')
 print('Fourth lesson about git and github')
 print("Today: ", datetime.today().strftime('%d/%m/%Y'))
