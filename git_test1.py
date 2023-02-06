fc7a05986a2fa656f8e6eb695893dfc42651ddb5
igor-pokataev <ikpokataev@gmail.com>
2023-02-06 15:27:32 +0500

    Add date

--- git_test1.py
+++ git_test1.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 name = input('Whats your name?')
 print('Hello, ' , name,'!')
 print('We learn git and github')
-print('Third lesson about git and github')
+print('Fourth lesson about git and github')
+print("Today: ", datetime.today().strftime('%d/%m/%Y')) 
